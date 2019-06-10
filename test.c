bdb35a105ad4908a740d836c5d5a4e1dbd8e4bad
Luigy Machaca <luigy.machaca@isabo.ai>
2019-06-10 01:28:53 -0300

    update

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    int arg1;
+    int arg2;
+    int arg3;
+    printf("asdfasfasfsd\n ");
+    scanf("%d %d %d",&arg1,&arg2,&arg3);
+
+    printf(">:%d %d %d",&arg1,&arg2,&arg3);
+
+
+    return 0;
+}
