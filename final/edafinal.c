99d24e8afdadf7acaa2b7ebfbc054915976ef4a5
Luigy Machaca <luigy.machaca@isabo.ai>
2019-06-18 09:30:23 -0300

    update

--- final/edafinal.c
+++ final/edafinal.c
@@ -950,11 +950,92 @@ void buscar_figura_MENUAG(TAG* tag, myTypeCmpFunc pCmpFunc){
     limpar_janela02();
 }
 
+
+
+void recorre_AGSALIDA(TAG* tag, FILE *fp){
+    if (!tag) return;
+
+    int cod, pai;    
+    int* parameters;
+
+    if (ler_cod_INFO(tag->info) == 1){
+        cod = 1;
+        pai = 0;
+        char* nome = ler_nome_INFO(tag->info);
+        parameters = ler_parameters_INFO(tag->info);
+        if (strcmp(nome,"TRI") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+        else if (strcmp(nome,"RET") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+        else if (strcmp(nome,"TRA") == 0) fprintf(fp,"%d/%d/%s %d %d %d\n",cod,pai,nome,parameters[0],parameters[1],parameters[2]);
+        else if (strcmp(nome,"CIR") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);
+        else if (strcmp(nome,"QUA") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);      
+    }
+
+    TAG* ant = NULL;
+    TAG* pos = NULL;
+    if (tag->primFilho){
+        pos = tag->primFilho;
+        ant = tag;
+        cod = ler_cod_INFO(pos->info);
+        pai = ler_cod_INFO(ant->info);
+        char* nome = ler_nome_INFO(pos->info);        
+        parameters = ler_parameters_INFO(pos->info);
+        if (strcmp(nome,"TRI") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+        else if (strcmp(nome,"RET") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+        else if (strcmp(nome,"TRA") == 0) fprintf(fp,"%d/%d/%s %d %d %d\n",cod,pai,nome,parameters[0],parameters[1],parameters[2]);
+        else if (strcmp(nome,"CIR") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);
+        else if (strcmp(nome,"QUA") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);
+
+        while(pos->proxIrmao){        
+            pos = pos->proxIrmao;
+            cod = ler_cod_INFO(pos->info);
+            char* nome = ler_nome_INFO(pos->info);
+            parameters = ler_parameters_INFO(pos->info);
+            if (strcmp(nome,"TRI") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+            else if (strcmp(nome,"RET") == 0) fprintf(fp,"%d/%d/%s %d %d\n",cod,pai,nome,parameters[0],parameters[1]);
+            else if (strcmp(nome,"TRA") == 0) fprintf(fp,"%d/%d/%s %d %d %d\n",cod,pai,nome,parameters[0],parameters[1],parameters[2]);
+            else if (strcmp(nome,"CIR") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);
+            else if (strcmp(nome,"QUA") == 0) fprintf(fp,"%d/%d/%s %d\n",cod,pai,nome,parameters[0]);
+        }
+    }        
+    
+    recorre_AGSALIDA(tag->primFilho,fp);
+    recorre_AGSALIDA(tag->proxIrmao,fp);
+}
+
+void imprimir_arquivo_AGSALIDA(TAG* tag, char* new_filename){
+    FILE *fp = fopen(new_filename, "w");
+    if (fp == NULL){
+        puts("Arquivo nao abierto\n");
+        return;
+    }
+    recorre_AGSALIDA(tag,fp);
+
+    fclose(fp);
+}
+
+
+void salvar_em_arquivo_MENUAG(TAG* tag, char* nomefile){
+    limpar_janela01();
+
+    printf("=======================================================================================\n");
+    printf(" Salvar em arquivo: %s \n",nomefile);
+
+    imprimir_arquivo_AGSALIDA(tag,nomefile);
+    
+    printf("=======================================================================================\n");
+
+    limpar_janela02();
+}
+
+
+
+
 void imprimir_arvore_MENUAG(TAG* tag){
     imprimirArquivo_AG_DOT(tag,"arvore_AG.dot");
     system("dot -Tps arvore_AG.dot -o arvore_AG.pdf");
     system("xdg-open arvore_AG.pdf");
 }
+
 
 TAG* inserir_figura_MENUAG(TAG* tag, myTypeCmpFunc pCmpFunc, int tipo){
     limpar_janela01();
@@ -2191,6 +2272,7 @@ int main(){
         printf("\t(h) Intercambiar figuras\n");
         printf("\t(i) Transformar a árvore genérica numa árvore AVL\n");
         printf("\t(j) Transformar a árvore genérica numa árvore B\n");
+        printf("\t(w) salvar em Arvore generico em arquivo\n");     
         printf("\t(k) Sair do programa\n");     
         printf("=======================================================================================\n");
         printf("Insira sua opção: \n");
@@ -2249,6 +2331,11 @@ int main(){
                 tab = inicializa_B();
                 tab = gerar_arvore_menuB(tag, tab);
                 tab = libera_B(tab);
+                getchar();
+                break;
+                
+            case 'w':
+                salvar_em_arquivo_MENUAG(tag,"arquivo_salvado.txt");
                 getchar();
                 break;
 
